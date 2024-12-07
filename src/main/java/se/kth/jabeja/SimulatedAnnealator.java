af7cc3b764e071f3dfb817a64d650367be21aaf7
JuliePolieOlie <144525122+JuliePolieOlie@users.noreply.github.com>
2024-12-07 20:29:40 +0100

    Create SimulatedAnnealator.java

---
+++ src/main/java/se/kth/jabeja/SimulatedAnnealator.java
@@ -0,0 +1,41 @@
+package se.kth.jabeja;
+import se.kth.jabeja.Node;
+import static java.lang.Math.exp;
+
+public class SimulatedAnnealator {
+    float Temp;
+    //float delta;
+    float alpha;
+
+    public SimulatedAnnealator(float temp) {
+        Temp = temp;
+        alpha = 0.9f;
+    }
+
+    public float getAlpha() {
+        return alpha;
+    }
+
+    public void setAlpha(float alpha) {
+        this.alpha = alpha;
+    }
+
+    public float getTemp() {
+        return Temp;
+    }
+
+    public void setTemp(float temp) {
+        Temp = temp;
+    }
+
+    public float updateTemp(){
+        Temp*=alpha;
+        return Temp;
+    }
+
+    public double acceptance_probability(double old_c, double new_c) {
+        double ap = exp((new_c-old_c)/Temp);
+        if (ap>1) return 1;
+        else return ap;
+    }
+}
